e6ac734abfd49f9c8b115914a3fe6331a487514c
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-03-08 20:59:36 +0100

    Add fake range
    The best thing about this is: if you import destruction into the ipython
    shell it crashes immediately.

--- destruction/__init__.py
+++ destruction/__init__.py
@@ -9,5 +9,10 @@ else:
 from destruction.FakeFloat import FakeFloat
 builtins.float = FakeFloat
 
+
 from destruction.FakeMath import FakeMath
 sys.modules['math'] = FakeMath()
+
+
+from destruction.FakeRange import fake_range
+builtins.range = fake_range
